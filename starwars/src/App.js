67f73c3b85d212fe9bda29d3d95fb31e21f3d43b
Impulse2020 <irishht@gmail.com>
2020-05-15 12:28:06 -0400

    Adds more states, and starts the axios

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,14 +1,33 @@
 import React, { useState } from 'react';
+import axios from "axios";
 import './App.css'
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [characters, setCharacters] = useState("");
-
+  const [characters, setCharacters] = useState([]);
+  const [ErrorState, setErrorState] = useState("");
+  const [isFetching, setIsFetching] = useState(false);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const api = 'https://swapi.py4e.com/api/people/';
+ // setIsFetching(true);
+  axios.get(api)
+  .then(response =>
+    console.log(response),
+  //  setIsFetching(false)
+    )
+  .catch(error => {
+
+
+  } )
+
+
+
+
+
 
   return (
     <div className="App">
